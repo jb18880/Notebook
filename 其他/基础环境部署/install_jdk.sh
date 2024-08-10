07251284f37e27dd628abf9f9f73dbe3ba4ba83b
Sekitonu <65084330+jb18880@users.noreply.github.com>
2024-08-10 14:26:40 +0800

    Update install_jdk.sh

--- 其他/基础环境部署/install_jdk.sh
+++ 其他/基础环境部署/install_jdk.sh
@@ -5,12 +5,14 @@
 #https://www.oracle.com/java/technologies/downloads/
 
 wget https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.tar.gz
+
+
 tar xf jdk-8u333-linux-x64.tar.gz -C /usr/local/
 ln -s /usr/local/jdk1.8.0_333/ /usr/local/jdk
-cat > /etc/profile.d/jdk.sh <<EOF
-export JAVA_HOME=/usr/local/jdk
-export PATH=\$PATH:\$JAVA_HOME/bin
+cat > /etc/profile.d/jdk.sh <<EOF    
+export JAVA_HOME=/usr/local/jdk  
+export PATH=\$PATH:\$JAVA_HOME/bin  
 EOF
-. /etc/profile.d/jdk.sh 
-java -version
-echo "jdk path = /usr/local/jdk"
+. /etc/profile.d/jdk.sh   
+java -version  
+echo "jdk path = /usr/local/jdk"  
