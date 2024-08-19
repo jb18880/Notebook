8d932812f2ef58725681608a465f3198ca0ef02a
Sekitonu <65084330+jb18880@users.noreply.github.com>
2024-08-19 12:56:55 +0800

    Update install_ansible.sh

--- 其他/基础环境部署/ubuntu安装ansible/install_ansible.sh
+++ 其他/基础环境部署/ubuntu安装ansible/install_ansible.sh
@@ -1,10 +1,10 @@
 apt -y install ansible
 cat >> /etc/ansible/hosts << EOF
 [webservers]
-10.0.0.8
+192.168.10.10
 EOF
 sed -i  's/#host_key_checking = False/host_key_checking = False/' /etc/ansible/ansible.cfg
 sed -i 's/#   StrictHostKeyChecking ask/    StrictHostKeyChecking no/' /etc/ssh/ssh_config
 ssh-keygen
-ssh-copy-id 10.0.0.8
-ansible 10.0.0.8 -m ping
+ssh-copy-id 192.168.10.10  
+ansible 192.168.10.10 -m ping
